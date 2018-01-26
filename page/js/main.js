fe3deedbe3a8e7f73b96a8ac3ad3361e31b38393
TashDubrovska <n.dubrovska89@gmail.com>
2018-01-26 20:22:42 +0000

    Optimise images, add lazy loading and minor UI fixes

--- page/js/main.js
+++ page/js/main.js
@@ -1,3 +1,30 @@
+// Lazy loading for background images
+var ll = document.querySelectorAll('div[class*=section]');
+var lh = [];
+var wh = window.innerHeight;
+
+function updateOffsets() {
+    ll.forEach(function(elem) {
+        lh.push(elem.offsetTop);
+    });
+};
+
+function lazy() {
+    wscroll = document.documentElement.scrollTop;
+    lh.forEach(function(elOffset, i) {
+        if(elOffset <= wscroll + (wh + 1000)){
+            ll[i].classList.add('loaded');
+        };
+    });
+};
+
+updateOffsets();
+lazy();
+
+window.addEventListener('scroll',function() {
+    lazy();
+});
+
 function UpdateUserImage(value,id){
     document.getElementById('img'+id).setAttribute("src",value);
 }
