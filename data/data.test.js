662d138ad449fd4c785b1ec53beabae79934c125
john681611 <john.harvey@marks-and-spencer.com>
2018-03-02 12:53:00 +0000

    Fix data and Data tests

--- data/data.test.js
+++ data/data.test.js
@@ -2,10 +2,10 @@ const data = require('./data');
 const httpMocks = require('node-mocks-http');
 const fs = require('fs');
 
-describe.only('File Mod Funcs', function () {
+describe('File Mod Funcs', function () {
     const authHeader = 'Basic ' + new Buffer('usr' + ':' + 'pwd').toString('base64');
     const file = './data/testOBJ.json';
-    let fsWriteStub, redirectStub;
+    let fsWriteStub, redirectStub, jsonStub;
     let res = {
         redirect: () => this,
         set: () => this,
@@ -16,19 +16,21 @@ describe.only('File Mod Funcs', function () {
     beforeEach(() => {
         fsWriteStub = sinon.stub(fs, 'writeFile').yields(null);
         redirectStub = sinon.stub(res, 'redirect');
+        jsonStub = sinon.stub(res, 'json');
     });
 
     afterEach(() => {
         fsWriteStub.restore();
         redirectStub.restore();
+        jsonStub.restore();
     });
 
-    describe.only('Save', function () {
+    describe('AddItem', function () {
 
-        it('should save bob to file', function () {
+        it('should save bob to empty file', function () {
             //given
             const expected = JSON.stringify([{
-                id: 0,
+                id: '0',
                 name: 'bob'
             }], null, 4);
             const req = httpMocks.createRequest({
@@ -41,25 +43,25 @@ describe.only('File Mod Funcs', function () {
                 }
             });
             //when
-            data.saveSomething(req, res, [], file);
+            data.addItem(req, res, [], file);
             //then
             expect(fsWriteStub.getCall(0).args[0]).to.equal(file);
             expect(fsWriteStub.getCall(0).args[1]).to.deep.equal(expected);
             expect(redirectStub).to.have.been.calledOnce;
         });
 
-        it('should save bob3 to file and be top of list', function () {
+        it('should save bob3 to file', function () {
             const expected = JSON.stringify([{
-                id: 0,
+                id: '0',
                 name: 'bob'
             },
             {
-                id: 1,
+                id: '1',
                 name: 'bob3'
             }], null, 4);
 
             const testOBJ = [{
-                id: 0,
+                id: '0',
                 name: 'bob'
             }];
 
@@ -73,23 +75,19 @@ describe.only('File Mod Funcs', function () {
                 }
             });
 
-            data.saveSomething(req, res, testOBJ, file, true);
+            data.addItem(req, res, testOBJ, file, true);
 
             expect(fsWriteStub.getCall(0).args[1]).to.deep.equal(expected);
         });
 
         it('should change bob to bob2', function () {
             const expected = JSON.stringify([{
-                id: 0,
+                id: '0',
                 name: 'bob2'
-            },
-            {
-                id: 1,
-                name: 'bob3'
             }], null, 4);
 
             const testOBJ = [{
-                id: 0,
+                id: '0',
                 name: 'bob'
             }];
 
@@ -103,20 +101,55 @@ describe.only('File Mod Funcs', function () {
                 }
             });
             //when
-            data.saveSomething(req, res, testOBJ, file);
+            data.addItem(req, res, testOBJ, file);
             //then
             expect(fsWriteStub.getCall(0).args[1]).to.deep.equal(expected);
         });
-    });
-    describe.only('Delete', function () {
-        let jsonStub;
-        beforeEach(()=>{
-            jsonStub = sinon.stub(res, 'json');
+
+        it('should handle index error', function () {
+
+            const testOBJ = [{
+                id: 1,
+                name: 'bob'
+            }];
+
+            const req = httpMocks.createRequest({
+                body: {
+                    id: '0'
+                },
+                headers: {
+                    'Authorization': authHeader
+                }
+            });
+            //when
+            data.deleteItem(req, res, testOBJ, file);
+            //then
+            expect(jsonStub).to.have.been.calledWith({error:'ID not found'});
         });
 
-        afterEach(()=>{
-            jsonStub.restore();
+        it('should handle fileWrite error', function () {
+            fsWriteStub.yields('fuck');
+            const testOBJ = [{
+                id: 1,
+                name: 'bob'
+            }];
+
+            const req = httpMocks.createRequest({
+                body: {
+                    id: '1'
+                },
+                headers: {
+                    'Authorization': authHeader
+                }
+            });
+            //when
+            data.deleteItem(req, res, testOBJ, file);
+            //then
+            expect(jsonStub).to.have.been.calledWith({error:'Something went wrong!'});
         });
+    });
+
+    describe('deleteItem', function () {
 
         it('should delete something from file', function () {
             const expected = JSON.stringify([], null, 4);
@@ -135,7 +168,7 @@ describe.only('File Mod Funcs', function () {
                 }
             });
             //when
-            data.deleteSomething(req, res, testOBJ, file);
+            data.deleteItem(req, res, testOBJ, file);
             //then
             expect(fsWriteStub.getCall(0).args[0]).to.equal(file);
             expect(fsWriteStub.getCall(0).args[1]).to.deep.equal(expected);
@@ -158,7 +191,7 @@ describe.only('File Mod Funcs', function () {
                 }
             });
             //when
-            data.deleteSomething(req, res, testOBJ, file);
+            data.deleteItem(req, res, testOBJ, file);
             //then
             expect(jsonStub).to.have.been.calledWith({error:'ID not found'});
         });
@@ -179,7 +212,7 @@ describe.only('File Mod Funcs', function () {
                 }
             });
             //when
-            data.deleteSomething(req, res, testOBJ, file);
+            data.deleteItem(req, res, testOBJ, file);
             //then
             expect(jsonStub).to.have.been.calledWith({error:'Something went wrong!'});
         });
@@ -187,13 +220,19 @@ describe.only('File Mod Funcs', function () {
 });
 
 describe('findIdx', function () {
-    var req = [{ id: 1 }, { id: 0 }];
+    var req = [{ id: 2 },{ id: '1' }, { id: '0' }];
     it('should find by ID not index', function () {
         //when
-        expect(data.findIdx(req, '1') === 0);
+        expect(data.findIdx(req, '1') === 1);
     });
+
     it('should return -1 when not found', function () {
         //when
-        expect(data.findIdx(req, '2') === -1);
+        expect(data.findIdx(req, '3') === -1);
+    });
+
+    it('should deal with num and string', function () {
+        //when
+        expect(data.findIdx(req, '2') === 0);
     });
 });
