72401ef0858f82b3ae606332effb339f352956fe
john681611 <john.harvey@marks-and-spencer.com>
2018-04-03 13:51:41 +0100

    allow to support editing

--- page/js/pwa.js
+++ page/js/pwa.js
@@ -1,21 +1,3 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
 
 /* eslint-env browser, es6 */
 
@@ -61,7 +43,13 @@ function updateBtn() {
 
 function updateSubscriptionOnServer(subscription) {
     if (subscription) {
-        $.post('/pwa', { sub: JSON.stringify(subscription) });
+        fetch('/pwa', {
+            method: 'post',
+            body: JSON.stringify(subscription),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            }
+        });
     }
 }
 
@@ -78,7 +66,7 @@ function subscribeUser() {
 
             updateBtn();
         })
-        .catch(function () {
+        .catch(function (err) {
             updateBtn();
         });
 }
