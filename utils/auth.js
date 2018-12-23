b36c22427c772d7f928be298b20a18da12714894
john681611 <john.harvey@marks-and-spencer.com>
2018-12-23 17:22:14 +0000

    webpack and service workers

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ const auth = require('basic-auth');
 
 const authenticate = (req, res, callback) => {
     var credentials = auth(req);
-    if (!credentials || credentials.name !== process.env.USR || credentials.pass !== process.env.pass) {
+    if (!credentials || credentials.name !== process.env.USR || credentials.pass !== process.env.PASS) {
         res.status(401);
         res.set('WWW-Authenticate', 'Basic realm="example"');
         res.end('Access denied');
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+
+
+module.exports = {
+    entry: ['./page/css/index.scss', './page/js/main.js'],
+    output: {
+        path: __dirname + '/assets',
+        publicPath: '/',
+        filename: 'js/[name].js',
+        chunkFilename: '[id].js'
+    },
+    devtool: 'source-map',
+    stats: {
+        children: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name].[ext]'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: '.postcssrc'
+                            },
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            includePaths: ['./pages/css']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: 'common',
+            minChunks: 2
+        },
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({}),
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: true
+                },
+                sourceMap: true
+            })
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({filename: 'css/[name].css'}),
+        new ManifestPlugin({
+            fileName: 'build-manifest.json',
+            publicPath: '/'
+        }),
+        new InjectManifest({swSrc: './page/js/sw.js'})
+    ]
+};
