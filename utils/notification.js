e940c0063bffc332970416d22f9d9f56dc15b75f
john681611 <john681611@hotmail.com>
2019-02-17 19:40:54 +0000

    delete rejected subs

--- utils/notification.js
+++ utils/notification.js
@@ -1,5 +1,7 @@
 const webpush = require('web-push');
 const data = require('../data/data');
+const fs = require('fs');
+
 if(process.env.vapidPu &&  process.env.vapidPr){
     webpush.setVapidDetails(
         'mailto:example@yourdomain.org',
@@ -8,22 +10,41 @@ if(process.env.vapidPu &&  process.env.vapidPr){
     );
 }
 
+const sendNotification = async (messageObj, sub, isMember = false) => {
+    try {
+        await webpush.sendNotification(sub, JSON.stringify(messageObj));
+    } catch (err) {
+        if(err.statusCode === 410) {
+            const subs = isMember? data.getMemberSubscriptions() : data.getPushSubscriptions();
+            const file = isMember?  './data/member/pushSubscriptions.json' : './data/pushSubscriptions.json';
+            const existingSubIndex = subs.findIndex(subscriber => sub.endpoint === subscriber.endpoint);
+            if(existingSubIndex != -1) {
+                subs.splice(existingSubIndex, 1);
+                fs.writeFileSync(file, JSON.stringify(subs, null, 4));
+                console.log('removed user');
+            }
+        } else {
+            console.error(err);
+        }
+    }
+}
 
 function notify(message, url) {
     data.getPushSubscriptions().forEach(sub =>{
-        webpush.sendNotification(sub, JSON.stringify({message, url})).catch(e => console.log(e)); //eslint-disable-line no-console
+        sendNotification({message, url}, sub);
     });
 }
 
 function notifyMembers(message, url) {
     data.getMemberSubscriptions().forEach(sub =>{
-        webpush.sendNotification(sub, JSON.stringify({message, url})).catch(e => console.log(e)); //eslint-disable-line no-console
+        sendNotification({message, url}, sub, true); //eslint-disable-line no-console
     });
 }
 
 function singleNotify(message, sub) {
-    webpush.sendNotification(sub, JSON.stringify({message, url:''})).catch(e => console.log(e)); //eslint-disable-line no-console
+    sendNotification({message, url: ''}, sub); //eslint-disable-line no-console
 }
+
 
 module.exports = {
     notify,
